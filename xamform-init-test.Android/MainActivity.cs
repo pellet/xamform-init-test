c756890f82de4ac68083efc2c005e71e236155b6
Ben Pettit <ben.pettit@smartcaptech.com>
2019-12-02 13:13:39 +1000

    added test project

---
+++ xamform-init-test.Android/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using Android.Util;
+
+namespace xamform_init_test.Droid
+{
+    [Activity(Label = "xamform_init_test", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        private static int onCreateCalled = 0;
+        private static readonly Lazy<App> app = new Lazy<App>(() => new App());
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            onCreateCalled++;
+            Log.Debug(nameof(MainActivity), $"OnCreateCalled:{onCreateCalled}");
+            LoadApplication(app.Value);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
